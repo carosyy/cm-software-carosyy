fb38451ab1f7a9cad06bf3f8bb0ec4e2d494de6a
carosyy <cmoraj@ucenfotec.ac.cr>
2025-05-25 00:20:01 -0600

    Item - Añadir getters and setters

--- Semana#2/Item.java
+++ Semana#2/Item.java
@@ -12,6 +12,37 @@ public class Item {
         this.quality = quality;
     }
 
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        if (itemName == null || itemName.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be null or empty. Please enter a valid name.");
+        }
+        this.itemName = itemName;
+    }
+
+    public int getItemSellIn() {
+        return itemSellIn;
+    }
+
+    public void setSellIn(int itemSellIn) {
+        this.itemSellIn = itemSellIn;
+    }
+
+    public int getitemQuality() {
+        return itemQuality;
+    }
+
+    public void setQuality(int itemQuality) {
+        if (itemQuality < 0 || itemQuality > 50) {
+            throw new IllegalArgumentException("Quality must be between 0 and 50. Please enter a valid quality.");
+        }
+        this.itemQuality = itemQuality;
+    }
+
+
    @Override
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
